c378557ac09845960f52223a90e98f47372fb19e
jafar1405 <jafar1405@gmail.com>
2023-10-18 06:35:45 +0530

    latest

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 //const http = require("http");
 const express = require("express");
 const fs = require("fs");
+// Require the bcrypt module
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// secret key
+const secret = "12345";
+
+// Generate a salt
+const salt = bcrypt.genSaltSync(10);
 
 const app = express();
 
@@ -10,33 +19,109 @@ app.use(express.json());
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// A function that creates a token and sends it to the response
+function createTokenAndSendResponse(user, res) {
+  // Create a payload object
+  const payload = {
+    id: user.id,
+    username: user.username,
+  };
+
+  // Sign the token
+  const token = jwt.sign(payload, secret, { expiresIn: "30m" });
+
+  // Send the token
+  res.json({ token: token });
+}
+
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-app.get("/users", function (req, res) {
+app.post("/signup", function (req, res) {
   fs.readFile("db.json", function (err, data) {
-    res.json(JSON.parse(data).users);
-  });
-});
-
-app.post("/users", function (req, res) {
-  fs.readFile("db.json", function (err, data) {
+    const { username, password } = req.body;
     const users = JSON.parse(data).users;
     const sortedUsersById = users.sort((a, b) => a.id - b.id);
     const autoIncrementId = sortedUsersById[sortedUsersById.length - 1].id + 1;
-    const newUser = { id: autoIncrementId, ...req.body };
+
+    // Hash the password with the salt
+    const hashPassword = bcrypt.hashSync(password, salt);
+
+    const newUser = { id: autoIncrementId, username, password: hashPassword };
     users.push(newUser);
     fs.writeFile(
       "db.json",
       JSON.stringify({ ...JSON.parse(data), users: users }),
       function (err) {
         if (err) throw err;
-        res.json(newUser);
+        // res.json(newUser);
+        createTokenAndSendResponse(newUser, res);
       }
     );
   });
 });
+
+app.post("/login", function (req, res) {
+  fs.readFile("db.json", function (err, data) {
+    const { username, password } = req.body;
+    const users = JSON.parse(data).users;
+
+    const foundUser = JSON.parse(data).users.find(
+      (user) => user.username === username
+    );
+    if (foundUser) {
+      // Compare the password "password" with the hash
+      const result = bcrypt.compareSync(password, foundUser.password);
+      if (result) {
+        createTokenAndSendResponse(foundUser, res);
+      } else {
+        res.json({ msg: "password not matching" });
+      }
+    } else {
+      res.json("No user found");
+    }
+  });
+});
+
+app.get("/users", function (req, res) {
+  const token = req.headers["x-access-token"];
+  if (token) {
+    // Validate the token
+    try {
+      // Verify the token and get the payload
+      const payload = jwt.verify(token, secret);
+
+      fs.readFile("db.json", function (err, data) {
+        res.json(JSON.parse(data).users);
+      });
+    } catch (err) {
+      // Handle the error
+      console.error(err);
+      res.json({ msg: "pleae provide valid token" });
+    }
+  } else {
+    res.json({ msg: "you are not authorized" });
+  }
+});
+
+// app.post("/users", function (req, res) {
+//   fs.readFile("db.json", function (err, data) {
+//     const users = JSON.parse(data).users;
+//     const sortedUsersById = users.sort((a, b) => a.id - b.id);
+//     const autoIncrementId = sortedUsersById[sortedUsersById.length - 1].id + 1;
+//     const newUser = { id: autoIncrementId, ...req.body };
+//     users.push(newUser);
+//     fs.writeFile(
+//       "db.json",
+//       JSON.stringify({ ...JSON.parse(data), users: users }),
+//       function (err) {
+//         if (err) throw err;
+//         res.json(newUser);
+//       }
+//     );
+//   });
+// });
 
 app.get("/users/:id", function (req, res) {
   fs.readFile("db.json", function (err, data) {
